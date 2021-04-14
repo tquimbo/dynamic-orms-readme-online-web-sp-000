4bc78b0092be9ee8b3e3293680bc9549bb017ce5
Tim Quimbo <tquimbo1@gmail.com>
2021-04-14 16:50:28 -0400

    commit tree 4361ecfd9da24b4ab48012827a44e3f4ccff826c

--- lib/song.rb
+++ lib/song.rb
@@ -14,13 +14,13 @@ class Song
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
-    column_names = []
+    column_names = []``
     table_info.each do |row|
       column_names << row["name"]
     end
     column_names.compact
   end
-
+``
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
